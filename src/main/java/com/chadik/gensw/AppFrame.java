11d556940894e8b135a04633e87bd7bfb954d8a7
android helios <android.helios@gmail.com>
2014-08-19 23:42:04 +0200

    I-02: added miglayout to pom file, poc for the main fraim of new layout

--- src/main/java/com/chadik/gensw/AppFrame.java
+++ src/main/java/com/chadik/gensw/AppFrame.java
@@ -1,16 +1,32 @@
 package com.chadik.gensw;
 
 import java.awt.BorderLayout;
+import java.awt.FlowLayout;
 
 import javax.swing.JFrame;
+
+import net.miginfocom.swing.MigLayout;
+
+import com.chadik.gensw.component.GenswLabel;
+import com.chadik.gensw.component.GenswLabelMandatory;
+import com.chadik.gensw.component.GenswTextField;
 
 public class AppFrame extends JFrame {
 
 	public AppFrame() {
+
 		setTitle("Gensw");
-		setLayout(new BorderLayout());
+		setLayout(new MigLayout("fillx,insets 1"));
 		setSize(1000, 750);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		GenswLabelMandatory genswLabelMandatory = new GenswLabelMandatory(
+				"Mandatory");
+		GenswTextField genswTextField = new GenswTextField("Text");
+
+		getContentPane().add(genswLabelMandatory, "width :100:, wrap");
+		getContentPane().add(genswTextField, "width :100:, growx");
+
 		setVisible(true);
 		setLocationRelativeTo(null);
 	}
